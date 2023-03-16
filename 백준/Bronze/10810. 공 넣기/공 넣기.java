124a820047de0d99866b52e55457962439a1d57d
Subin An <81276634+richsubin@users.noreply.github.com>
2023-03-16 10:34:17 +0900

    [Bronze III] Title: 공 넣기, Time: 140 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] basket = new int[N];
+		
+		for(int cnt = 0;cnt<M;cnt++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			for(int ball=i-1;ball<j;ball++) {
+				basket[ball] = k;
+			}
+			
+		}
+		
+		for(int b:basket) bw.write(String.valueOf(b)+" ");
+		bw.flush();
+		bw.close();
+	}
+}
