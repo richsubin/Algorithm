03b3904fa05666f0623dbe7ca696b582c65b9ffa
Subin An <81276634+richsubin@users.noreply.github.com>
2023-03-30 19:03:27 +0900

    [Gold IV] Title: 플로이드, Time: 748 ms, Memory: 46228 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m;
+	static int[][] inputInfo;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		inputInfo = new int[n+1][n+1];
+		
+		for(int k=0;k<m;k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			
+			if(inputInfo[i][j]==0)
+				inputInfo[i][j] = Integer.parseInt(st.nextToken());
+			else {
+				int temp = Integer.parseInt(st.nextToken());
+				if(temp < inputInfo[i][j]) 
+					inputInfo[i][j] = temp;
+			}
+		}
+		
+		for(int a =1;a<=n;a++) {
+			for(int x=1;x<=n;x++) {
+				for(int y=1;y<=n;y++) {
+					if(x == a || y == a||x==y) continue;
+					if(inputInfo[x][a]==0||inputInfo[a][y]==0)
+						continue;
+					if(inputInfo[x][y]==0)
+						inputInfo[x][y]=inputInfo[x][a]+inputInfo[a][y];
+					else
+						inputInfo[x][y] = Math.min(inputInfo[x][y], inputInfo[x][a]+inputInfo[a][y]);			
+				}
+			}
+		}
+		
+		for(int a=1;a<=n;a++) {
+			for(int b=1;b<=n;b++) {
+				System.out.print(inputInfo[a][b]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+
+}
