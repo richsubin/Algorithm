773b391614d089bb671529d90f2288b489af0323
Subin An <81276634+richsubin@users.noreply.github.com>
2023-04-15 18:58:21 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 140 ms, Memory: 15808 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long n = Long.parseLong(br.readLine()); //시간복잡도 n^2
+		bw.write((n*n*n)+"\n"+3);
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
