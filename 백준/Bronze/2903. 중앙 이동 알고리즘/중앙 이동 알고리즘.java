c20e20b540fc5445a4c08403d905a54394e59a7f
Subin An <81276634+richsubin@users.noreply.github.com>
2023-06-20 00:20:09 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		int k = 2;
+		int answer = 0;
+		for(int i=0;i<n;i++) {
+			k = 2*k-1;
+			answer = k*k;
+		}
+		bw.write(answer+"");
+		br.close();
+		bw.flush();
+		bw.close();	
+	}
+}
