f96d8bc9d3f236c5a14a213cc3de8f6e71bf989e
Subin An <81276634+richsubin@users.noreply.github.com>
2023-07-16 12:07:47 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,41 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            String str = br.readLine();
-            sb.append(check(str) + "\n");
-        }
-        bw.write(sb+"");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-        public static String check(String str){
-            Stack<Character> stack = new Stack<Character>();
 
-                for(int j = 0;j<str.length();j++){
-                    char c = str.charAt(j);
-                    if(c=='('){
-                        stack.push(c);
-                    }
-                    else{
-                        if(stack.isEmpty()) {
-                            return "NO";
-                        }
-                        else stack.pop();
-                    }
-                }
-            if(stack.isEmpty()){
-                return "YES";
-            }
-            else{
-                return "NO";
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int cnt = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<cnt;i++) {
+			String str = br.readLine();
+			Stack<Character> stack = new Stack<>();
+			Boolean vps = true;
+			for(int j=0;j<str.length();j++) {
+				char c = str.charAt(j);
+				
+				if(c=='(') {
+					stack.push(c);
+				}
+				else if(stack.empty()) {
+					vps = false;
+					break;
+				}
+				else {
+					stack.pop();
+				}
+			}
+			if(!stack.isEmpty()||!vps) {
+				sb.append("NO\n");
+			}
+			else {
+				sb.append("YES\n");
+			}
+			
+		}
+		
+		bw.write(sb+"");
+		bw.flush();
+		bw.close();
+	}
+
+}
