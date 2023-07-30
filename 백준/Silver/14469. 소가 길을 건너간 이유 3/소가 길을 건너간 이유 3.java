14bad9ecdde8382b3b4e382bb1743e276ed18ef2
Subin An <81276634+richsubin@users.noreply.github.com>
2023-07-30 15:23:05 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -1,64 +1,32 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-class Cow implements Comparable<Cow>{
-	int arrive;
-	int enter;
-
-	public Cow(int arrive, int enter) {
-		super();
-		this.arrive = arrive;
-		this.enter = enter;
-	}
-	
-	//도착시간으로 정렬해줘야함 만약 도착 시간 같으면 검문시간 짧은애부터인가...?
-	@Override
-	public int compareTo(Cow c) {
-		return this.arrive != c.arrive? this.arrive-c.arrive : this.enter-c.enter;
-	}
-}
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static Cow[] cows;
-	static int N;
-	static int result = 0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException  {
 
-		N = Integer.parseInt(br.readLine());
-		cows = new Cow[N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			cows[i] = new Cow(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
-		}
-		
-		getResult();
-		
-		bw.write(result+"");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	public static void getResult() {
-		Arrays.sort(cows);
-		int endTime = 0;
-		for(int i=0;i<N;i++) {
-			if(endTime< cows[i].arrive) {
-				endTime = cows[i].arrive + cows[i].enter;
-			}
-			else if(endTime >= cows[i].arrive) {
-				endTime += cows[i].enter;
-			}
-		}
-		result = endTime;
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int endTime = 0;
+        int cowArr[][] = new int[N][2]; //[n][0] 도착시간, [n][1] 검문시간
+
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cowArr[i][0] = Integer.parseInt(st.nextToken());
+            cowArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(cowArr, (o1,o2)-> o1[0]-o2[0]);
+        
+        for(int i=0;i<N;i++){
+            if(endTime < cowArr[i][0]){
+                endTime=cowArr[i][0]+cowArr[i][1];
+            }
+            else
+                endTime += cowArr[i][1];
+        }
+        bw.write(endTime+"");
+        bw.flush();
+        bw.close();
+    }
 }
