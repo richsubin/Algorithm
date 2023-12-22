93a14a2842b8c12aaca6200a61815c10448fa2c2
Subin An <81276634+richsubin@users.noreply.github.com>
2023-12-22 10:55:03 +0900

    [Gold IV] Title: 수 분해, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/1437. 수 분해/수 분해.java
+++ 백준/Gold/1437. 수 분해/수 분해.java
@@ -1,32 +1,28 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		int num = Integer.parseInt(br.readLine());
+		int[] result = new int[num+1];
 		
-		int N = Integer.parseInt(br.readLine());
-		
-		long[] arr = new long[N+1];
-		
-		for(int i=0;i<=N;i++) {
-			if(i<=4) {
-				arr[i] = i;
-			}
-			else {
-				arr[i] = (arr[i-3] *3)%10007;				
-			}
+		if(num<5) {
+			System.out.print(num);
 		}
 		
-		bw.write(arr[N]+"");
-		bw.flush();
-		bw.close();
-		br.close();
-
+		
+		else {
+			for(int i = 0;i<5;i++) {
+				result[i] = i;
+			}
+			
+			for(int i = 5;i<=num;i++) {
+				result[i] = (result[i-3] * 3)%10007;
+			}
+			
+			System.out.print(result[num]);
+			
+		}
 	}
 }
