b9cb7167af2b4b71e59c4cc88af00a407f7556a8
Subin An <81276634+richsubin@users.noreply.github.com>
2024-03-18 22:03:15 +0900

    [Silver II] Title: 스택 수열, Time: 28 ms, Memory: 1972 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.c
+++ 백준/Silver/1874. 스택 수열/스택 수열.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define SIZE 100000
- 
-char result[SIZE * 2];
-int stack[SIZE];
+#define MAX_SIZE 100000
+
+// global variables
+int stack[MAX_SIZE];
 int top = -1;
- 
+
 int main() {
     int n;
     scanf("%d", &n);
- 
-    //목표 수열 입력
+
     int* arr = (int*)malloc(sizeof(int) * n);
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
- 
-    int num = 1;
-    int idx = 0, result_idx = 0;
-    while (1) {
-        //스택에 있는 수가 목표 수 보다 낮은 경우
-        //push(+)
-        if (top == -1 || stack[top] < arr[idx]) {
-            stack[++top] = num++;
-            result[result_idx++] = '+';
+
+    int cur = 1;
+    int arr_idx = 0;
+    char result_arr[MAX_SIZE * 2];
+    int res_idx = 0;
+
+    while (arr_idx < n) {
+        if (top ==  -1 || stack[top] < arr[arr_idx]) {
+            while (cur <= n && cur <= arr[arr_idx]) {
+                stack[++top] = cur++;
+                result_arr[res_idx++] = '+';
+            }
         }
-        //스택 상단부분의 수가 목표 수와 같은 경우
-        //pop(-)
-        else if (stack[top] == arr[idx]) {
+        
+        else if (stack[top] == arr[arr_idx]) {
             top--;
-            result[result_idx++] = '-';
-            idx++;
+            result_arr[res_idx++] = '-';
+            arr_idx++;
         }
-        //스택의 상단부분의 수가 목표 수 보다 높은경우
-        //원하는 수열을 만들 수 없음
+        
         else {
             printf("NO\n");
             return 0;
         }
-        if (result_idx == n * 2) break;
     }
-    
-    //결과 출력
-    for (int i = 0; i < result_idx; i++)
-        printf("%c\n", result[i]);
- 
+
+    for (int i = 0; i < res_idx; i++)
+        printf("%c\n", result_arr[i]);
+
     return 0;
 }
