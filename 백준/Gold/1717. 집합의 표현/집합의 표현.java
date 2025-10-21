e5d7c87531ff3a02cafec548642111ebcde8a389
Subin An <81276634+richsubin@users.noreply.github.com>
2025-10-21 13:10:03 +0900

    [Gold V] Title: 집합의 표현, Time: 420 ms, Memory: 49740 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,61 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-	static int n, m;
-	static int[] parents;
-	static StringBuilder sb;
 	
+	static int n,m;
+	static int[] sets;
+	static StringBuilder sb;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		sb = new StringBuilder();
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
 		
-		parents = new int[n+1];
+		StringTokenizer st =  new StringTokenizer(br.readLine());
 		
-		for(int i=0;i<n+1;i++) {
-			parents[i] = i;
+		n = Integer.parseInt(st.nextToken()); //count of sets
+		m = Integer.parseInt(st.nextToken()); //count of operations
+		sets = new int[n+1];
+		
+		for(int i =0;i<n+1;i++) { //initializing sets' values
+			sets[i] = i;
 		}
 		
 		for(int i=0;i<m;i++) {
 			st = new StringTokenizer(br.readLine());
-			int calc = Integer.parseInt(st.nextToken());
+			int op = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			if(calc == 0) {
-				union(a,b);
-			}
-			else {
-				isSame(a,b);
-			}
+			if(op == 1) isSet(a, b);
+			else unionSet(a, b);
 		}
 		
-		System.out.print(sb);
+		bw.write(sb+"");
+		bw.flush();
+		bw.close();
 	}
 	
-	static int find(int x) {
-		if(parents[x] == x) return x;
-		return parents[x] = find(parents[x]);
-	}
-	
-	static void union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot == bRoot) return;
-		parents[bRoot] = aRoot;
-	}
-	
-	static void isSame(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot == bRoot) sb.append("YES\n");
+	static void isSet(int a, int b) {
+		if(findSet(a) == findSet(b)) {
+			sb.append("YES\n");
+		}
 		else sb.append("NO\n");
 	}
 	
+	static int findSet(int num) {
+		if(sets[num] == num) return num;
+		
+		return sets[num] = findSet(sets[num]);
+	}
+	
+	static void unionSet(int a, int b) {
+		//이미 union 일경우
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		if(aRoot == bRoot) return;
+		
+		//해야하는 경우
+		sets[bRoot] = aRoot;
+	}
 }
